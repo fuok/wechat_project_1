ed655848863c7193e28ca9ce4db568838bf5074e
zzk1st <zzk1st2015@qq.com>
2018-06-30 11:03:29 +0800

    Fixed gitignore

--- assets/Test Game/scripts/EnemyManager.js
+++ assets/Test Game/scripts/EnemyManager.js
@@ -22,21 +22,6 @@ let EnemyManager = cc.Class({
             default: null,
             type: cc.Prefab
         },
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
     },
 
     // LIFE-CYCLE CALLBACKS:
